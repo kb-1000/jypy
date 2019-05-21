fbcc783828bdfe341235318ec64e0a1f8a6f08f6
kb1000 <kaeptmblaubaer1000@gmail.com>
2019-05-21 07:22:17 +0200

    Add new methods to create PyUnicode2 objects from Java Strings using either UTF-16 or UCS-2 to decode them

--- src/main/java/com/github/kb1000/jypy/PyUnicode2.java
+++ src/main/java/com/github/kb1000/jypy/PyUnicode2.java
@@ -1,8 +1,11 @@
 package com.github.kb1000.jypy;
 
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.IntBuffer;
 import java.nio.ShortBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.github.kb1000.jypy.common.Array;
 
@@ -30,6 +33,76 @@ public class PyUnicode2 extends PyObject {
         this.kinds = kinds;
     }
 
+    public static PyUnicode2 fromUCS2(String ucs2) {
+        ByteBuffer buf = ByteBuffer.allocate(ucs2.length() * 2);
+        buf.asCharBuffer().put(CharBuffer.wrap(ucs2.toCharArray())); // TODO(kb1000): test whether this actually works, on BE and on LE
+        return new PyUnicode2(new ByteBuffer[] { buf }, new int[] { 2 });
+    }
+
+    public static PyUnicode2 fromUTF16(String utf16) {
+        char[] chars = utf16.toCharArray();
+        int len = chars.length;
+        ByteBuffer buf = ByteBuffer.allocate(len * 2);
+        boolean hasSurrogates = false;
+        int i = 0;
+        for (; i < len; i++) {
+            char character = chars[i];
+            if (Character.isHighSurrogate(character)) {
+                hasSurrogates = true;
+                break;
+            }
+            buf.putShort(i*2, (short) character);
+        }
+        if (!hasSurrogates) {
+            return new PyUnicode2(new ByteBuffer[] { buf }, new int[] { 2 });
+	} else {
+            List<ByteBuffer> slices = new ArrayList<>();
+            int start = 0;
+            boolean startsWithSurrogate = i == 0;
+            // TODO(kb1000): check correctness of indices here
+            for (; i < len; i++) {
+                char character = chars[i];
+                if (Character.isHighSurrogate(character)) {
+                    char character2 = chars[i+1];
+                    if (Character.isLowSurrogate(character2)) {
+                        if (i != 0) {
+                            slices.add(buf.duplicate().position(start * 2).limit(i * 2).slice());
+                            start = i;
+                        }
+                        buf.putInt(i * 2, Character.toCodePoint(character, character2));
+                        i += 2;
+                        while (Character.isHighSurrogate(chars[i]) && Character.isLowSurrogate(chars[i+1])) {
+                            buf.putInt(i * 2, Character.toCodePoint(chars[i], chars[i + 1])); // TODO(kb1000): read the characters only once instead of twice
+                            i += 2;
+                        }
+                        slices.add(buf.duplicate().position(start * 2).limit(i * 2).slice());
+                        start = i;
+                        i--;
+                    } else {
+                        buf.putShort(i++ * 2, (short) character);
+                        buf.putShort(i * 2, (short) character2);
+                    }
+                } else {
+                    buf.putShort(i * 2, (short) character);
+                }
+            }
+            if (start != i) slices.add(buf.duplicate().position(start * 2).limit(i * 2).slice());
+            final int slice_count = slices.size();
+            int[] kinds = new int[slice_count];
+            if (startsWithSurrogate) {
+                final int j_max = slice_count + 1;
+                for (int j = 1; j < j_max; j++) {
+                    kinds[j-1] = ((j % 2) + 1) * 2;
+                }
+            } else {
+                for (int j = 0; j < slice_count; j++) {
+                    kinds[j] = ((j % 2) + 1) * 2;
+                }
+            }
+            return new PyUnicode2(slices.toArray(new ByteBuffer[slice_count]), kinds);
+        }
+    }
+
     private void evaluate() {
         if (evaluated) return;
         synchronized (lock) {
@@ -50,7 +123,7 @@ public class PyUnicode2 extends PyObject {
             }
             ByteBuffer result;
             //if (resultLen < MAXIMUM_HEAP) {
-                result = ByteBuffer.allocate(resultLen);
+                result = ByteBuffer.allocate(resultLen * resultKind);
             //} else {
             //    result = ByteBuffer.allocateDirect(resultLen);
             //}
@@ -93,7 +166,7 @@ public class PyUnicode2 extends PyObject {
                         break;
                     case 2:
                         ShortBuffer shortBuffer = buffer.asShortBuffer();
-                        while (buffer.hasRemaining()) {
+                        while (shortBuffer.hasRemaining()) {
                             intResult.put(shortBuffer.get() & ((int) 0xFFFF));
                         }
                         break;
